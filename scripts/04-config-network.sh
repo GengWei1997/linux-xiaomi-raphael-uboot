51afccf11206a6480d5d8aafc3a185530784ba34
cuicanmx <arl96075@163.com>
2026-04-27 22:05:45 +0800

    fix(网络配置): 确保正确创建并配置resolv.conf文件
    删除旧的resolv.conf文件并重新创建，确保DNS配置正确应用

--- scripts/04-config-network.sh
+++ scripts/04-config-network.sh
@@ -6,6 +6,10 @@ NAMESERVER="${NAMESERVER:-1.1.1.1}"
 
 echo "[04] 配置网络和主机名"
 
+rm -f rootdir/etc/resolv.conf
+touch rootdir/etc/resolv.conf
+
+# 配置网络
 echo "nameserver ${NAMESERVER}" > rootdir/etc/resolv.conf
 echo "${HOSTNAME}" > rootdir/etc/hostname
 echo "127.0.0.1 localhost
