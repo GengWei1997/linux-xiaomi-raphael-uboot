928c4c55e2e4e9022b8243597439e9f687b51ad9
cuicanmx <arl96075@163.com>
2026-04-28 12:44:09 +0800

    fix(apt-setup): 修复apt源文件中的EOF引号问题
    移除apt源配置中的单引号以正确解析变量

--- scripts/05-apt-setup.sh
+++ scripts/05-apt-setup.sh
@@ -8,14 +8,14 @@ export DEBIAN_FRONTEND=noninteractive
 cp rootdir/etc/apt/sources.list rootdir/etc/apt/sources.list.bak
 
 if [[ "$SYSTEM_TYPE" == *"ubuntu-"* ]]; then
-    cat > rootdir/etc/apt/sources.list << 'EOF'
+    cat > rootdir/etc/apt/sources.list << EOF
 deb http://ports.ubuntu.com/ubuntu-ports/ $UBUNTU_VERSION main restricted universe multiverse
 deb http://ports.ubuntu.com/ubuntu-ports/ $UBUNTU_VERSION-updates main restricted universe multiverse
 deb http://ports.ubuntu.com/ubuntu-ports/ $UBUNTU_VERSION-backports main restricted universe multiverse
 deb http://ports.ubuntu.com/ubuntu-ports/ $UBUNTU_VERSION-security main restricted universe multiverse
 EOF
 else
-    cat > rootdir/etc/apt/sources.list << 'EOF'
+    cat > rootdir/etc/apt/sources.list << EOF
 deb http://deb.debian.org/debian/ $DEBIAN_VERSION main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian/ $DEBIAN_VERSION-updates main contrib non-free non-free-firmware
 deb http://deb.debian.org/debian/ $DEBIAN_VERSION-backports main contrib non-free non-free-firmware
