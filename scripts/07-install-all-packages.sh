189214a25e4506a6c9fe81a1381456c80a9c1f39
cuicanmx <arl96075@163.com>
2026-04-28 14:17:31 +0800

    fix(scripts): 调整基础包安装列表，修复netplan.io缺失问题
    在Debian系统中添加netplan.io包以确保网络配置功能正常

--- scripts/07-install-all-packages.sh
+++ scripts/07-install-all-packages.sh
@@ -19,10 +19,10 @@ echo "[$(date +'%Y-%m-%d %H:%M:%S')] [06]   └─ 更新系统包..."
 chroot rootdir apt update
 chroot rootdir apt upgrade -y
 
-BASE_PACKAGES="bash-completion netplan sudo apt-utils ssh openssh-server nano network-manager initramfs-tools chrony curl wget locales tzdata iproute2"
+BASE_PACKAGES="bash-completion sudo apt-utils ssh openssh-server nano network-manager initramfs-tools chrony curl wget locales tzdata iproute2"
 
 if [[ "$SYSTEM_TYPE" == *"debian-"* ]]; then
-    BASE_PACKAGES="$BASE_PACKAGES systemd-boot dnsmasq fonts-wqy-microhei"
+    BASE_PACKAGES="$BASE_PACKAGES systemd-boot dnsmasq netplan.io fonts-wqy-microhei"
 elif [[ "$SYSTEM_TYPE" == *"ubuntu-"* ]]; then
     BASE_PACKAGES="$BASE_PACKAGES systemd-boot dnsmasq language-pack-zh-hans iptables"
 fi
